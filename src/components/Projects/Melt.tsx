a0c67d9ec8a42e966f6a987045dbbbb103e4fcdc
gargansa <gargansa@users.noreply.github.com>
2023-11-06 15:44:40 -0700

    adjusted MELT descriptions

--- src/components/Projects/Melt.tsx
+++ src/components/Projects/Melt.tsx
@@ -6,15 +6,14 @@ import BasicCarousel from './Carousel'
 export default class Melt extends Component {
   state = {
       slides: [
-        ['https://i.imgur.com/bMBWVuc.jpeg','Small Boat with random color adjustments between blue and red every 4-8 random layers gives an excellent banding.'],
-        ['https://i.imgur.com/LoV9brW.jpeg','Bottom of vase with random color adjustments between white and black every 2-4 layers. Effect is weighted towards white so that the black does not overpower the results.'],
+        ['https://i.imgur.com/bMBWVuc.jpeg','Melt allows a mixing nozzle to change between a red filament and a blue filament at varying percentages. In this example every 4-8 layers a new random percentage is chosen.'],
+        ['https://i.imgur.com/LoV9brW.jpeg','Some colors of filaments such as black contain an overpowering amount of pigments. The MELT script contains a modifier that will allow the favor one percentage of the mixture over the other. This results in more gray banding where it would normally be mostly black.'],
         ['https://i.imgur.com/c5b5QNW.jpeg','Vase with random changes every 2-4 random layers between red and blue to bring out range of purples.'],
         ['https://i.imgur.com/AHS7JOz.jpeg','Voronoi pencil holder, black and white with random changes every 4-8 layers.'],
-        ['https://i.imgur.com/fymr8km.jpeg','Pencil holder gray and white. Settings were random changes every 1-2 layers.'],
+        ['https://i.imgur.com/fymr8km.jpeg','Pencil holder, gray and white. Settings were random changes every 1-2 layers.'],
         ['https://i.imgur.com/J88kqis.jpeg','Gradient Pencil holder adjustments every 4 layers to slowly shift towards white.'],
-        ['https://i.imgur.com/NUiBW5Z.jpeg','Flexi Rex with slow shift from blue to red.'],
+        ['https://i.imgur.com/NUiBW5Z.jpeg','Flexi Rex with slow shift from blue to red. Each layer is seperated by a change in percentage of color.'],
         ['https://i.imgur.com/hTvScjP.jpeg','Door Sign with single adjustment halfway through the print to shift from white to gray.']
-        
     ]
   }
 
